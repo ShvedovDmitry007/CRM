ef52a5df542f085a4c41a14a461908c1bed38a90
Дмитрий Шведов <shvedovd007@yandex.ru>
2024-07-04 14:04:53 +0500

    add HW module 3

--- js-copy/main.js
+++ js-copy/main.js
@@ -1,15 +1,18 @@
 'use strict';
 
-const productName = String(prompt('Наименование товара'));
-const productCount = Number(prompt('Количество товара'));
-const productСategory = String(prompt('Категория товара'));
-const productPrice = Number(prompt('Цена товара'));
+// Первая задача:
+const productName = prompt('Наименование товара');
+const productCount = +prompt('Количество товара');
+const productСategory = prompt('Категория товара');
+const productPrice = +prompt('Цена товара');
 const totalPrice = productCount * productPrice;
 
-console.log(typeof productName);
-console.log(typeof productCount);
-console.log(typeof productСategory);
-console.log(typeof productPrice);
+// Сравниваем полученые результаты, введённые пользователем. 
+// Если пользователь вводит число в оба поля, то это true и выводится сообщение о товаре в консоль. 
+// Если вводит другие символы, то это false и выводится в консоль ошибка.
 
-console.log(`На складе имеется ${productCount} единицы товара "${productName}" категории "${productСategory}" на общую сумму: ${totalPrice} долларов. Цена за одну единицу товара: $ ${productPrice}`);
-
+if (productCount > 0 && productPrice > 0) {
+  console.log(`На складе имеется ${productCount} единицы товара "${productName}" категории "${productСategory}" на общую сумму: ${totalPrice} долларов. Цена за одну единицу товара: $ ${productPrice}`);
+} else {
+  console.log('Вы ввели некорректные данные');
+};
--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 // 'use strict';
 
-// const productName = "Игровая консоль PlayStation 5";
-// const productCount = 5;
-// const productСategory = "Электроника";
-// const productPrice = 670;
-// const totalPrice = productCount * productPrice; 
+const productName = "Игровая консоль PlayStation 5";
+const productCount = 5;
+const productСategory = "Электроника";
+const productPrice = 670;
+const totalPrice = productCount * productPrice; 
 
 
-// console.log(productName);
-// console.log(`Общая сумма товара: $ ${totalPrice}`);
+console.log(productName);
+console.log(`Общая сумма товара: $ ${totalPrice}`);
